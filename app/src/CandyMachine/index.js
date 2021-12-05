673aa6fb12e6248ece08b2c2b76054fb14aa7925
Gilles Taupenas <g@international-web-services.com>
2021-12-05 22:37:43 +0200

    Mint working

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -324,7 +324,7 @@ const CandyMachine = ({ walletAddress }) => {
       <div className="machine-container">
         <p>{`Drop Date: ${machineStats.goLiveDateTimeString}`}</p>
         <p>{`Items Minted: ${machineStats.itemsRedeemed} / ${machineStats.itemsAvailable}`}</p>
-        <button className="cta-button mint-button" onClick={null}>
+        <button className="cta-button mint-button" onClick={mintToken}>
             Mint NFT
         </button>
       </div>
