0a6355e8ad4f1a51dc32c0041a03b307cb8e7b76
Gilles Taupenas <g@international-web-services.com>
2021-12-06 21:49:50 +0200

    Added dropDate counter

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -4,6 +4,7 @@ import { Program, Provider, web3 } from '@project-serum/anchor';
 import { MintLayout, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
 import { programs } from '@metaplex/js';
 import './CandyMachine.css';
+import CountdownTimer from '../CountdownTimer';
 import {
   candyMachineProgram,
   TOKEN_METADATA_PROGRAM_ID,
@@ -364,6 +365,23 @@ const CandyMachine = ({ walletAddress }) => {
     </div>
   );
 
+  // Create render function
+  const renderDropTimer = () => {
+    // Get the current date and dropDate in a JavaScript Date object
+    const currentDate = new Date();
+    const dropDate = new Date(machineStats.goLiveData * 1000);
+
+    // If currentDate is before dropDate, render our Countdown component
+    if (currentDate < dropDate) {
+      console.log('Before drop date!');
+      // Don't forget to pass over your dropDate!
+      return <CountdownTimer dropDate={dropDate} />;
+    }
+    
+    // Else let's just return the current drop date
+    return <p>{`Drop Date: ${machineStats.goLiveDateTimeString}`}</p>;
+  };
+
   useEffect(() => {
     getCandyMachineState();
   }, []);	
@@ -374,7 +392,9 @@ const CandyMachine = ({ walletAddress }) => {
     // Only show this if machineStats is available
     machineStats && (
       <div className="machine-container">
-        <p>{`Drop Date: ${machineStats.goLiveDateTimeString}`}</p>
+        {/* Add this at the beginning of our component */}
+        {renderDropTimer()}
+  
         <p>{`Items Minted: ${machineStats.itemsRedeemed} / ${machineStats.itemsAvailable}`}</p>
         <button 
           className="cta-button mint-button" 
