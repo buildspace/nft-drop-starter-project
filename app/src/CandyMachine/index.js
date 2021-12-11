859b15783cc094f5982c474c7654fc7ce1f03b71
Kurt Karpov <kurtkarpov@Kurts-MacBook-Pro.local>
2021-12-11 01:16:38 -0600

    its kinda working

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -25,8 +25,54 @@ const MAX_SYMBOL_LENGTH = 10;
 const MAX_CREATOR_LEN = 32 + 1 + 1;
 
 const CandyMachine = ({ walletAddress }) => {
-
   // Actions
+  useEffect(() => {
+    getCandyMachineState();
+  }, []);
+
+const getProvider = () => {
+  const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST;
+
+  const connection = new Connection(rpcHost);
+
+  const provider = new Provider(
+    connection,
+    window.solana,
+    opts.preflightCommitment
+  );
+
+  return provider;
+};
+
+const getCandyMachineState = async () => {
+  const provider = getProvider();
+
+  const idl = await Program.fetchIdl(candyMachineProgram, provider);
+
+  const program = new Program(idl, candyMachineProgram, provider);
+
+  const candyMachine = await program.account.candyMachine.fetch(
+    process.env.REACT_APP_CANDY_MACHINE_ID
+  );
+
+  const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
+  const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
+  const itemsRemaining = itemsAvailable - itemsRedeemed;
+  const goLiveData = candyMachine.data.goLiveDate.toNumber();
+
+  const goLiveDateTimeString = `${new Date(
+    goLiveData * 1000
+  ).toGMTString()}`
+
+  console.log({
+    itemsAvailable,
+    itemsRedeemed,
+    itemsRemaining,
+    goLiveData,
+    goLiveDateTimeString,
+  });
+};
+
   const fetchHashTable = async (hash, metadataEnabled) => {
     const connection = new web3.Connection(
       process.env.REACT_APP_SOLANA_RPC_HOST
@@ -249,64 +295,7 @@ const CandyMachine = ({ walletAddress }) => {
       programId: SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
       data: Buffer.from([]),
     });
-    useEffect(() => {
-      getCandyMachineState();
-    }, []);	
-
-    const getProvider = () => {
-      const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST;
-      // Create a new connection object
-      const connection = new Connection(rpcHost);
-      
-      // Create a new Solana provider object
-      const provider = new Provider(
-        connection,
-        window.solana,
-        opts.preflightCommitment
-      );
-    
-      return provider;
-    };
-
-    // Declare getCandyMachineState as an async method
-const getCandyMachineState = async () => { 
-  const provider = getProvider();
-  
-  // Get metadata about your deployed candy machine program
-  const idl = await Program.fetchIdl(candyMachineProgram, provider);
-
-  // Create a program that you can call
-  const program = new Program(idl, candyMachineProgram, provider);
-
-  // Fetch the metadata from your candy machine
-  const candyMachine = await program.account.candyMachine.fetch(
-    process.env.REACT_APP_CANDY_MACHINE_ID
-  );
-  
-  // Parse out all our metadata and log it out
-  const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
-  const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
-  const itemsRemaining = itemsAvailable - itemsRedeemed;
-  const goLiveData = candyMachine.data.goLiveDate.toNumber();
-
-  // We will be using this later in our UI so let's generate this now
-  const goLiveDateTimeString = `${new Date(
-    goLiveData * 1000
-  ).toGMTString()}`
-
-  console.log({
-    itemsAvailable,
-    itemsRedeemed,
-    itemsRemaining,
-    goLiveData,
-    goLiveDateTimeString,
-  });
-};
-
-
-  }
-
-  
+  };
 
   return (
     <div className="machine-container">
@@ -317,8 +306,6 @@ const getCandyMachineState = async () => {
       </button>
     </div>
   );
-}
-
-
+};
 
 export default CandyMachine;
