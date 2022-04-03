1e7ee03024bf3bcb440eb79c9c7f1bf3c8a1a174
Masaya Funakoshi <masayafunakoshi1@gmail.com>
2022-04-03 02:25:12 -0400

    Added wallet connection

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,12 +7,73 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  //State
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  //Actions
+    //Declare check for Phantom Wallet function
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const {solana} = window;
+
+      if(solana && solana.isPhantom){
+        console.log('Phantom Wallet Found!');
+
+        //connect wallet and confirm with pubkey
+        const response = await solana.connect({onIfTrusted: true})
+        console.log(
+          'Connected with Public Key: ',
+          response.publicKey.toString()
+        );
+
+        //set the users pubkey into walletAddress
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const connectWallet = async() => {
+    const {solana} = window;
+
+    if(solana){
+      const response = await solana.connect();
+      console.log('Connected with Public Key: ', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderWhenNotConnected = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}>
+        Connect to Wallet
+      </button>
+  )
+
+
+  //When our component first mounts, check if we have connected Phantom Wallet
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    }
+
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
+
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && renderWhenNotConnected()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
