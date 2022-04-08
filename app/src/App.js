ed1fc50f3ac6e2fb9354b606310425a1fbd3b94c
Chanakya Valluri <chanakyav@github.com>
2022-04-08 17:44:28 -0400

    Built the `Connect the Wallet` button

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,22 +7,31 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  // Actions
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
       
-      if (solana && solana.isPhantom) {
-        console.log('Phantom wallet found!');
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
 
-        /**
-         * The solana object gives us a function that will allow us
-         * to connect directly with the user's wallet! 
-         */
-        const response = await solana.connect({onlyIfTrusted: true});
-        console.log(
-          'Connected with Public Key:',
-          response.publicKey.toString()
-        );
+          /**
+           * The solana object gives us a function that will allow us
+           * to connect directly with the user's wallet! 
+           */
+          const response = await solana.connect({onlyIfTrusted: true});
+          const publicKeyString = response.publicKey.toString();
+          console.log('Connected with Public Key:', publicKeyString);
+
+          /**
+           * Set the user's publickey in state to be used later!
+           */
+          setWalletAddress(publicKeyString);
+        }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
       }
@@ -30,6 +39,34 @@ const App = () => {
       console.error(error);
     }
   };
+
+  /**
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      const publicKeyString = response.publicKey.toString();
+      console.log('Connected with Public Key:', publicKeyString);
+      setWalletAddress(publicKeyString)
+    }
+  };
+
+  /**
+   * We want to render this UI when the user hasn't connected 
+   * their wallet to our app yet.
+   */
+  const rendeNotConnectedContainer = () => (
+    <button
+      className='cta-button connect-wallet-button'
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
 
   /**
    * When our component first mounts, let's check to see if we have a 
@@ -51,6 +88,7 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && rendeNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
