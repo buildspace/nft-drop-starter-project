6fe1a6782f2183b850a8ef5a1636607aa8ebf584
Ian DC <jokerzamba@hotmail.com>
2022-07-13 17:23:58 -0700

    Check for solana object in window object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,33 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  // Check window object in DOM: has Phantom Wallet Ext. injected 
+  // solana object?
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana && solana.isPhantom) {
+        console.log('Phantom Wallet found!')
+      } else {
+        alert('Solana object not found --> Get a Phantom Wallet!')
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // When component first mounts, check if Phantom Wallet is connected
+  useEffect( () => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected()
+    }
+    // Listen for event: window object load --> call onLoad
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
