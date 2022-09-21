5793fca528f332459411bcf9133ff2ab6143e8af
Sean <seanlengws@gmail.com>
2022-09-21 13:10:35 +0800

    Updated next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    env: {
-        REACT_APP_CANDY_MACHINE_ID: "YOUR_CANDY_MACHINE_ID",
-        REACT_APP_SOLANA_NETWORK: "devnet",
-        REACT_APP_SOLANA_RPC_HOST: "https://metaplex.devnet.rpcpool.com/",
-    },
 };
 
 module.exports = nextConfig;
