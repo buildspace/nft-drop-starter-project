012f6c3a1b484e8f3ccbf14d7fa6289ec8e17045
bstar826 <tayasehambeger@gmail.com>
2024-03-15 05:20:08 -0700

    Add: nft-mint-function

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,19 +1,49 @@
 import React from "react";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+import twitterLogo from "./../public/twitter-logo.svg"
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+
+
+// This is to disable SSR when using WalletMultiButton
+const WalletMultiButtonDynamic = dynamic(
+  async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+// Constants
 const Home = () => {
+    const wallet = useWallet();
+    // Actions
+    const renderNotConnectedContainer = () => (
+        <div>
+            <Image src={twitterLogo} alt="emoji" />
+
+            <div className="button-container">
+                <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+            </div>
+        </div>
+    );
+
     return (
         <div className="App">
             <div className="container">
                 <div className="header-container">
                     <p className="header">🍭 Candy Drop</p>
                     <p className="sub-text">NFT drop machine with fair mint</p>
+                    {/* Render your connect to wallet button right here */}
+                    {renderNotConnectedContainer()}
+                    {/* {wallet.publicKey ? "Hello World" : renderNotConnectedContainer()} */}
                 </div>
+
                 <div className="footer-container">
-                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+                    <Image alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
                     <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
                 </div>
             </div>
